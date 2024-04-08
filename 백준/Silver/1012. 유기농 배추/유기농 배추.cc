3573c9fbbf97aa757cd1037af3724d73d35ad0df
Yudility <yudility@gmail.com>
2024-04-08 17:59:01 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,45 +1,57 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1}; 
-int m, n, k, y, x, ret, ny, nx, t;
+
+int t, k, y, x, ny, nx, ret, n, m; //t 테케 개수, k 배추 개수
+
+int dy[4]= {-1, 0, 1, 0};
+int dx[4]= {0, -1, 0, 1};
 int a[51][51];
 bool visited[51][51]; 
+
 void dfs(int y, int x){
     visited[y][x] = 1;
-    for(int i = 0; i < 4; i++){
-        ny = y + dy[i];
-        nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny >=n || nx >= m) continue;
-        if(a[ny][nx] == 1 && !visited[ny][nx]){
+    for(int i = 0; i<4; i++){
+        ny= y + dy[i];
+        nx= x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m ) continue;
+        if(!visited[ny][nx] && a[ny][nx]==1 ){
             dfs(ny, nx);
-        }
+        } 
     }
     return;
 }
 
-int main(){ 
+int main(){
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> t;
-    while(t--){
+
+    cin >> t; 
+    for(int i=0; i<t; i++){
+
         fill(&a[0][0], &a[0][0] + 51 * 51, 0);
         fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
-        ret = 0;
+
+        ret =0;
         cin >> m >> n >> k;
-        for(int i = 0; i < k; i++){
+        
+        for(int j=0; j<k ; j++){
             cin >> x >> y;
             a[y][x] = 1;
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(a[i][j] == 1 && !visited[i][j]){
-                    dfs(i, j);
+        
+        for(int h=0 ; h<n; h++){
+            for(int l=0; l<m; l++ ){
+                if(visited[h][l]==0 && a[h][l]==1){
+                    dfs(h,l);
                     ret++;
                 }
             }
         }
-        cout << ret << "\n"; 
+
+        cout << ret << "\n";
     }
+
     return 0;
 }
