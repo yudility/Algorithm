e670813d7f7247e2c33127b863123d08256aa23e
Yudility <yudility@gmail.com>
2025-09-15 11:03:40 +0900

    [level 3] Title: 단어 변환, Time: 0.77 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,7 +2,16 @@ import java.util.*;
 
 class Solution {
     
-    static int answer = Integer.MAX_VALUE;
+    // BFS에서 사용할 노드 클래스
+    class Node {
+        int index;  // words 배열에서의 인덱스
+        int steps;  // 변환 횟수
+        
+        Node(int index, int steps) {
+            this.index = index;
+            this.steps = steps;
+        }
+    }
     
     public boolean canChange(String from, String to){
         if(from.equals(to)){
@@ -16,22 +25,6 @@ class Solution {
             }
         }
         return diff == 1;
-    }
-    
-    public void dfs(int now, String target, String[] words, boolean[] visited, int step){
-        visited[now] = true;
-        
-        if(words[now].equals(target)){
-            answer = Math.min(answer, step);
-            return;
-        }
-        
-        
-        for(int i=0; i<words.length ; i++){
-            if(!visited[i] && canChange(words[now], words[i])){
-                dfs(i, target, words, visited, step+1);
-            }
-        }
     }
     
     public int solution(String begin, String target, String[] words) {
@@ -48,17 +41,42 @@ class Solution {
         if(isNotFound){
             return 0;
         }
-
-        //2. 변환할 수 있는 경우
+        
+        //2. BFS로 변환 경로 찾기
+        boolean[] visited = new boolean[words.length];
+        Queue<Node> q = new LinkedList<>();
+        
+        // begin에서 한 번에 변환 가능한 단어들을 큐에 추가
         for(int i=0; i<words.length ; i++){
             if(canChange(begin, words[i])){
-                boolean[] visited = new boolean[words.length];
-                dfs(i, target ,words, visited, 1);
+                q.offer(new Node(i, 1));
+                visited[i] = true;
+                
+                // target에 바로 도달한 경우
+                if(words[i].equals(target)){
+                    return 1;
+                }
             }
         }
         
-        return answer;
+        while(!q.isEmpty()){
+            Node current = q.poll();
+            
+            // 현재 단어에서 변환 가능한 단어들 탐색
+            for(int i=0; i<words.length ; i++){
+                if(!visited[i] && canChange(words[current.index], words[i])){
+                    visited[i] = true;
+                    
+                    // target에 도달한 경우
+                    if(words[i].equals(target)){
+                        return current.steps + 1;
+                    }
+                    
+                    q.offer(new Node(i, current.steps + 1));
+                }
+            }
+        }
+        
+        return 0; // 변환 불가능한 경우
     }
-    
-
-}                        
+}
